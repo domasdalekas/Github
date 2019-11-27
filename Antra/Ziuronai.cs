c25c11a5cb7bd3a403034072939d14eb94392f88
domasdalekas <48487114+domasdalekas@users.noreply.github.com>
2019-11-27 15:09:31 +0200

    Naujos klasės

---
+++ Antra/Ziuronai.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antra
+{
+    class Ziuronai : Daiktas
+    {
+        public int Priartinimas { get; }
+        public int Diametras { get; }
+        public Ziuronai(int priartinimas, int diametras):base(pavadinimas,kaina)
+        {
+            this.Priartinimas = priartinimas;
+            this.Diametras = diametras;
+        }
+    }
+}
